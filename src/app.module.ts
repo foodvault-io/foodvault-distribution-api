7a4f222b62971ed62f8d9af8d0b97459b3768d1a
Miguel Chevres <miguel.chevres@foodvault.io>
2023-03-31 01:36:46 -0400

    Created Restaurant Resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { UsersModule } from './users/users.module';
     }),
     AuthModule,
     UsersModule,
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [
